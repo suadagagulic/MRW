b9532c3d7747a3e6435240827e9bf57a3a443503
suadagagulic <s_gagulic2@fanshaweonline.ca>
2017-04-14 00:28:34 -0400

    Add files via upload

---
+++ js/whContent.js
@@ -0,0 +1,44 @@
+var contentSwapper =
+{
+	whoWeAre:
+	{
+		title: "WHO WE ARE",
+		profilePic1: "",
+		name1: "",
+		role1: "",
+		paragraph: "We are designers, thinkers, communicators, creators and more. We strive to help companies communicate through stunning designs and graphics. At Westhouse Digital we are passionate about what we do and care about those we do business with! We think outside the box and offer fresh creative ideas for your design needs.",
+		profilePic2: "",
+		name2: "",
+		role2: "",
+		paragraph2: ""
+	},
+
+	whatWeDo:
+	{
+		title: "WHAT WE DO",
+		profilePic1: "",
+		name1: "",
+		role1: "",
+		paragraph: "At Westhouse Digital we work together to create beautiful logos and other forms of marketing. We listen to the clients needs and work to bring their ideas to life. We are graphic designers, front-end developers and marketers! We get messages across through unique and beautiful visuals.",
+		profilePic2: "",
+		name2: "",
+		role2: "",
+		paragraph2: ""
+	},
+
+	team:
+	{
+		title: "OUR TEAM",
+		profilePic1: "<img src='images/justin-portrait.jpg'> ",
+		name1: "Justin West",
+		role1: "Web Developer",
+		paragraph: "Justin West is an extremely hardworking, determined member of our team. His passion for front-end developing clearly shows in his extrodinary work. He strives to create unique websites that meet every clients needs. He is constantly keeping up with new technologies and works hard to be the best he can be.",
+		profilePic2: "<img src='images/meredith-portrait.jpg'> ",
+		name2: "Meredith House",
+		role2: "Graphic Designer",
+		paragraph2: "Meredith House is a motivated, creative and out of the box thinker that we here at Westhouse Digital value. Her love of design pushes her to create stunning visuals of all sorts. She excells in the arts and impresses every client she comes across."
+	}
+
+
+
+};
